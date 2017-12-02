eca368e3a196810a6c190424aa286d0c2b7eccc1
Andrey Mileshin <andrej.mileshin@yandex.ru>
2017-12-03 00:22:53 +0300

    added delete_tables script

---
+++ src/sql/delete_tables.sql
@@ -0,0 +1,36 @@
+---------------------------------------------------------
+-- DROP TABLE
+---------------------------------------------------------
+DROP TABLE BRIGADE_PEOPLE_LINK;
+DROP TABLE DRIVING_LICENCE;
+DROP TABLE REPAIRS;
+DROP TABLE BREAKDOWN;
+DROP TABLE BUSES;
+DROP TABLE SCHEDULE;
+DROP TABLE ROUTES;
+DROP TABLE BRIGADES;
+DROP TABLE DRIVERS;
+DROP TABLE PEOPLE;
+DROP TABLE POSITIONS;
+DROP TABLE TYPES_OF_BUSES;
+
+---------------------------------------------------------
+-- DROP SEQUENCE
+---------------------------------------------------------
+DROP SEQUENCE driving_licence_seq;
+DROP SEQUENCE repairs_seq;
+DROP SEQUENCE breakdown_seq;
+DROP SEQUENCE buses_seq;
+DROP SEQUENCE shedule_seq;
+DROP SEQUENCE routers_seq;
+DROP SEQUENCE brigades_seq;
+DROP SEQUENCE drivers_seq;
+DROP SEQUENCE people_seq;
+DROP SEQUENCE position_seq;
+DROP SEQUENCE type_buses_seq;
+
+---------------------------------------------------------
+-- DROP TYPE
+---------------------------------------------------------
+DROP TYPE NAME_TYPE;
+DROP TYPE CHARACTERISTICS_BUS_TYPE;
